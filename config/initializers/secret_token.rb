8382593b326ac1afe696f2088e53d19702357f82
insub <insub@126.com>
2013-03-14 13:50:07 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erhuamao::Application.config.secret_token = '964a3195f5d2711af8a8fb9904816d105409d1d9255c6e96515ae3e7e7fa0d32665df6545721ffece7e48bbe4863637d658af4108006bd3e8cc34483c3bcfc33'
