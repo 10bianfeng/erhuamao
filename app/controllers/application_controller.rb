28f29383ef86f0efa5f0263530d2d36aef19f04a
insub <insub@126.com>
2013-04-08 17:50:15 +0800

    base style and some init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def require_admin
+    redirect_to "/404.html" unless current_user && Setting.admin_emails.include?(current_user.email)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -43,19 +43,27 @@ Erhuamao::Application.routes.draw do
   #   end
 
   # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  namespace :admin do
+    #root :to => "admin#index"
+    resources :users do
+      member do
+        get :lock, :unlock, :generate_new_password_email
+      end
+    end
+    resources :travels
+  end
+
+
+  get 'admin' => 'admin/base#index'
+  get 'admin/handle' => 'admin/base#handle'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
