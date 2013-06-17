d3367948ac84f72061e7f1f70bc57696227e24b3
cming.xu <cming.xu@gmail.com>
2013-06-17 14:02:10 +0800

    correct migration error

--- db/migrate/20130522065322_fix_column_name.rb
+++
@@ -1,9 +0,0 @@
-class FixColumnName < ActiveRecord::Migration
-  def self.up
-    rename_column :pages, :type, :page_type
-  end
-
-  def self.down
-  	rename_column :pages, :page_type, :type_of_page
-  end
-end
