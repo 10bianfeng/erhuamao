9b2d26fa725517d6d525b61e4d82e1dfea7a1e5a
Kevin.Xu <cming.xu@gmail.com>
2013-08-08 14:34:31 +0800

    fuck

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-Erhuamao::Application.config.session_store :cookie_store, :key => '_erhuamao_session', :expire_after => 3.hours
+Erhuamao::Application.config.session_store :cookie_store, :key => '_erhuamrehgeurghreugrao_session', :expire_after => 3.hours
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
