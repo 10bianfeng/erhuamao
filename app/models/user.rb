f0bde154efc17466bcdd71f13f218d3bb0ee947f
Kevin.Xu <cming.xu@gmail.com>
2013-08-11 11:09:40 +0800

    no cn in user file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, # 基本数据库
-         #:token_authenticatable, # 
-         #:omniauthable, # 社会化登录
-         #:timeoutable, #
-         :registerable, # 注册
-         :async, # 异步发送
-         :recoverable, # 找回密码
-         :rememberable, # Remember
-         :trackable, # 用户登录IP等追踪记录
-         :validatable # 校验字段
-         #:lockable#, # 禁用解锁用户
-         #:confirmable # 再次（邮件激活）确认才可登录
+    #:token_authenticatable, # 
+    #:omniauthable, # 社会化登录
+    #:timeoutable, #
+    :registerable, # 注册
+    :async, # 异步发送
+    :recoverable, # 找回密码
+    :rememberable, # Remember
+    :trackable, # 用户登录IP等追踪记录
+    :validatable # 校验字段
+  #:lockable#, # 禁用解锁用户
+  #:confirmable # 再次（邮件激活）确认才可登录
 
   # Setup accessible (or protected) attributes for your model
   #attr_accessible :email, :password, :password_confirmation, :remember_me
@@ -31,12 +31,12 @@ class User < ActiveRecord::Base
   has_many :dianpins
 
   #def after_database_authentication
-    当用户成功登录后会执行此处代码，另有登录后到那个路径的方法
+  #当用户成功登录后会执行此处代码，另有登录后到那个路径的方法
   #end
 
   # 是否是管理员
   def admin?
     Setting.admin_emails.include?(self.email)
   end
-  
+
 end
